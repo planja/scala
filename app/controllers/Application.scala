6d9ba4eb86e754b4d42574ee26f8933e19e5a01c
Mikita Shchykalau <ShchykalauM@iba>
2017-01-09 11:37:10 +0300

    Initial commit

---
+++ app/controllers/Application.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import play.api.mvc._
+
+object Application extends Controller {
+
+  def index = Action {
+    Ok(views.html.index("Your new application is ready."))
+  }
+
+  def test=Action {
+    Ok(views.html.test("test"))
+  }
+
+}
